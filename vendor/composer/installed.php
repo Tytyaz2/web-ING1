17ac20db68352ca4ab4abc2e3e24766678bd97bc
CYTech Student <soares.flavio2002@gmail.com>
2024-04-11 22:54:00 +0200

    CSS menu exemple

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'votre/nom-projet',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6262f6df3c1b8737d5ef2cc41f7601bb48b11aea',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.1',
+            'version' => '6.9.1.0',
+            'reference' => '039de174cd9c17a8389754d3b877a2ed22743e18',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'votre/nom-projet' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6262f6df3c1b8737d5ef2cc41f7601bb48b11aea',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
