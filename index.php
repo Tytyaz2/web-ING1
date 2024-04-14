5131fbe68f9b785cb4016ec5259031dfea1ac5ec
Gabriel <gabrielbrossat@gmail.com>
2024-04-15 00:18:24 +0200

    page bar

--- index.php
+++ index.php
@@ -6,14 +6,9 @@ include "php/haut.inc.php";
 //element de connexion
 include "php/page_connexion.inc.php";
 //menu pour changer la page
-include "php/menu.inc.php";
+include_once "php/menu.inc.php";
 //corps de la page avec l'affichage des produits si on cllique dessus sur le menu
-if (!isset($_GET["categorie"])){
-    include "php/page_bienvenue.inc.php";
-}
-else {
-    include "php/produit.inc.php";
-}
+include "php/produit.inc.php";
 //formulaire de contact
 include "php/contact.inc.php";
 //copyright
