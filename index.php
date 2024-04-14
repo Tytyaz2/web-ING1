f96fd51aa582e519a1dcfdeece9919acd75babfa
CYTech Student <soares.flavio2002@gmail.com>
2024-04-14 23:44:36 +0200

    index avec bienvenue

--- index.php
+++ index.php
@@ -6,9 +6,14 @@ include "php/haut.inc.php";
 //element de connexion
 include "php/page_connexion.inc.php";
 //menu pour changer la page
-include_once "php/menu.inc.php";
+include "php/menu.inc.php";
 //corps de la page avec l'affichage des produits si on cllique dessus sur le menu
-include "php/produit.inc.php";
+if (!isset($_GET["categorie"])){
+    include "php/page_bienvenue.inc.php";
+}
+else {
+    include "php/produit.inc.php";
+}
 //formulaire de contact
 include "php/contact.inc.php";
 //copyright
