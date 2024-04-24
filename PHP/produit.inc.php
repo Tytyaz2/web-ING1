2b9a67ba701c1b5848cc5c7104729e8c3fd1294e
CYTech Student <soares.flavio2002@gmail.com>
2024-04-24 14:37:15 +0200

    page produit + ajout du panier

--- PHP/produit.inc.php
+++ PHP/produit.inc.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="../CSS/produit.css">
+<script src="../JS/produit..js"></script>
 <div id="produit">
     <?php
 
@@ -13,10 +14,19 @@
             if ($resultat->rowCount() > 0) {
 
                 while($row = $resultat->fetch(PDO::FETCH_ASSOC)) {
-                    echo "<div class='zoom-container'>
-                    <img class='zoom-img' src='/IMAGE/".$row["nom"].".png' alt='".$row["nom"]."'>
-                    <div class='text-container'>".$row["nom"]." - ".$row["prix"]."€<br>".$row["description"]."</div>
-                    </div>";
+                    echo "<div class='produit'>
+                            <div class='zoom-container'>
+                                <img class='zoom-img' src='/IMAGE/".$row["nom"].".png' alt='".$row["nom"]."'>
+                                <div class='text-container'>".$row["nom"]." - ".$row["prix"]."€<br>".$row["description"]."</div>
+                            </div>";
+                    echo '  <div class="panier"> 
+                                <input type="number" id="quantity_'.$row["id"].'" name="quantity_'.$row["id"].'" min="1" value="1" style="width: 30px; margin-left: 20px">
+                                <button onclick="decreaseQuantity('.$row["id"].')">-</button>
+                                <button onclick="increaseQuantity('.$row["id"].')">+</button>
+                                <button onclick="addToCart('.$row["id"].')">Ajouter au panier</button>
+                            </div>
+                         </div>';
+
                 }
             } else {
                 echo "Aucun résultat trouvé";
