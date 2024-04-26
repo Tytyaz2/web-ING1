d9830f614db2999315000b622f25dac3dfc074c2
Epsi574 <pillotpier@cy-tech.fr>
2024-04-26 15:38:26 +0200

    description

--- produit (1).sql
+++ produit (1).sql
@@ -41,21 +41,21 @@ CREATE TABLE IF NOT EXISTS `produit` (
 --
 
 INSERT INTO `produit` (`id`, `nom`, `prix`, `stock`, `description`, `type`) VALUES
-(1, 'Pinky Promise', 1500, 12, 'Cette bière vous promettra une nuit folle (mais ça reste entre vous)', 'Rose'),
-(2, 'Spicy Sip', 1000, 11, 'Une bière qui vous réserve de piquantes surprises', 'Rose'),
-(3, 'Crazy Craft', 800, 20, 'Une bière dont la confection artisanale rendra vos soirées géniales', 'Rose'),
-(4, 'Cherry Cheers', 900, 15, 'Son goût aux cerises titillera vos papilles', 'Rose'),
-(5, 'Berlin Booze', 1700, 5, 'Cette bière saura vous faire penser aux sempiternelles soirées de Berlin', 'Rose'),
-(6, 'Fresh Foam', 1000, 17, 'La fraicheur de cette bière égale même les eaux les plus pures', 'Bleue'),
-(7, 'Drowned Drink', 1050, 35, 'Laissez vous noyer sous une avalanche de saveurs', 'Bleue'),
-(8, 'Calm Cup', 860, 33, 'De quoi passer un moment des plus agréables', 'Bleue'),
-(9, 'OceanOil', 510, 65, 'Une bière à consommer sur la plage... ou en plein milieu des océans', 'Bleue'),
-(10, 'RomeAroma', 1000, 18, 'Une bière qui vous rappelera le calme des rues de Rome, la ville éternelle', 'Bleue'),
-(11, 'Bestial Beverage', 700, 5, 'Une bière qui fera de vous une bête', 'Verte'),
-(12, 'Wild Wheat', 140, 40, 'Une bière qui ravivera vos instincts les plus sauvages', 'Verte'),
-(13, 'Guilty Glass', 600, 20, 'Ne vous sentez pas coupable de ne boire plus que ça', 'Verte'),
-(14, 'Jungle Jar', 10, 21, 'Les instincts de la jungle surgiront de cette bière', 'Verte'),
-(15, 'Lagos Lager', 30, 58, 'La boire vous fera vous sentir dans les rues de la belle Lagos', 'Verte');
+(1, 'Pinky Promise',2.99, 12, 'Cette bière vous promettra une nuit folle (mais ça reste entre vous)', 'Rose'),
+(2, 'Spicy Sip', 4.99, 11, 'Une bière qui vous réserve de piquantes surprises', 'Rose'),
+(3, 'Crazy Craft', 4.49, 20, 'Une bière dont la confection artisanale rendra vos soirées géniales', 'Rose'),
+(4, 'Cherry Cheers', 3.99, 15, 'Son goût aux cerises titillera vos papilles', 'Rose'),
+(5, 'Berlin Booze', 4.99, 5, 'Cette bière saura vous faire penser aux sempiternelles soirées de Berlin', 'Rose'),
+(6, 'Fresh Foam', 2.49, 17, 'La fraicheur de cette bière égale même les eaux les plus pures', 'Bleue'),
+(7, 'Drowned Drink', 2.99, 35, 'Laissez vous noyer sous une avalanche de saveurs', 'Bleue'),
+(8, 'Calm Cup', 2.49, 33, 'De quoi passer un moment des plus agréables', 'Bleue'),
+(9, 'OceanOil', 3.99, 65, 'Une bière à consommer sur la plage... ou en plein milieu des océans', 'Bleue'),
+(10, 'RomeAroma', 4.99, 18, 'Une bière qui vous rappelera le calme des rues de Rome, la ville éternelle', 'Bleue'),
+(11, 'Bestial Beverage', 3.49, 5, 'Une bière qui fera de vous une bête', 'Verte'),
+(12, 'Wild Wheat', 4.49, 40, 'Une bière qui ravivera vos instincts les plus sauvages', 'Verte'),
+(13, 'Guilty Glass', 2.49, 20, 'Ne vous sentez pas coupable de ne boire plus que ça', 'Verte'),
+(14, 'Jungle Jar', 3.99, 21, 'Les instincts de la jungle surgiront de cette bière', 'Verte'),
+(15, 'Lagos Lager', 4.99, 58, 'La boire vous fera vous sentir dans les rues de la belle Lagos', 'Verte');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
